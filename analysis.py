b07d24a4942e3aa9f9ee6196a8c727905ceeec8c
Daniyalatjit <Daniyalatjit@users.noreply.github.com>
2020-06-09 16:26:46 +0530

    Created Get Response Function

--- analysis.py
+++ analysis.py
@@ -1,1 +1,19 @@
-ECHO is on.
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import requests
+
+
+def getResponse():
+    url = "https://covid-193.p.rapidapi.com/statistics"
+
+    headers = {
+        'x-rapidapi-host': "covid-193.p.rapidapi.com",
+        'x-rapidapi-key': "d977ce9798msh4e5fe441381dc13p1addf4jsn5ad3eadb6644"
+    }
+
+    response = requests.request("GET", url, headers=headers)
+    return getResponse().json()['response']
+
+data = getResponse()
+print(data)
