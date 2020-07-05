71b66074106028b36ab57118bd0e0955f01192f1
Daniyalatjit <Daniyalatjit@users.noreply.github.com>
2020-07-05 18:03:02 +0530

    Project Pre Final Commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# CoronaWebApp
-It is a web app specially made for corona spread prediction and visualization using flask.
--- models.py
+++ models.py
@@ -1,112 +1,143 @@
-import pandas as pd
+# math operations
+# import math
+# produce random numbers
+# import random
+# to load json files
+import json
+# datetime operations
+from datetime import timedelta
+# to get web contents
+# from urllib.request import urlopen
+
+# for numerical analyiss
 import numpy as np
+# to store and process data in dataframe
+import pandas as pd
+
+# basic visualization package
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import requests
+# advanced ploting
+import seaborn as sns
+
+# interactive visualization
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly
+# import plotly.figure_factory as ff
+from plotly.subplots import make_subplots
+
+# for offline ploting
+# from plotly.offline import plot, iplot, init_notebook_mode
+
+# init_notebook_mode(connected=True)
+
+# converter
+# from pandas.plotting import register_matplotlib_converters
+# register_matplotlib_converters()
+
+# hide warnings
+import warnings
+
+warnings.filterwarnings('ignore')
+
+# to USA states details
+# import us
+
+# color pallette
+cnf, dth, rec, act = '#ffbf47', '#6f0000', '#0f9b0f', '#1cb5e0'
 
 
-def growthVisualizer(url):
+def fullData(url):
+    full_table = pd.read_csv(url)
+
+
+def groupedData(url):
+    grouped_table = pd.read_csv(url)
+    grouped_table['Date'] = pd.to_datetime(grouped_table['Date'])
+
+
+def ratio_dayWise(url):
+    day_wise = pd.read_csv(url)
+    day_wise['Date'] = pd.to_datetime(day_wise['Date'])
+    temp = day_wise[['Date', 'Deaths', 'Recovered', 'Active']].tail(1)
+    temp = temp.melt(id_vars='Date', value_vars=['Active', 'Deaths', 'Recovered'])
+    fig = [
+        go.Treemap(temp, path=['variable'], values='value', height=225, color_discrete_sequence=[act, rec, dth])
+    ]
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    print(graphJSON)
+    # return graphJSON
+
+
+def case_over_time_map(url, col, title):
     df = pd.read_csv(url)
 
-    def dataHead(self):
-        return df.head(10)
-
-    def plot(self, label):
-        plt.style.use('seaborn-bright')
-        plt.plot(cases['ObservationDate'], cases['Confirmed'], 'y')
-        plt.plot(cases['ObservationDate'], cases['Deaths'], 'r')
-        plt.plot(cases['ObservationDate'], cases['Recovered'], 'g')
-        plt.plot(cases['ObservationDate'], cases['Active'], 'c')
-        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
-        plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=5))
-        plt.gcf().autofmt_xdate()
-        plt.title(label=label)
-        plt.ylabel('Cases')
-        plt.xlabel('Dates')
-        return plt
-
-    country = df['Country_Region'].unique()
-    cases = df.groupby('ObservationDate')
-    result = []
-    for k, v in df.groupby('ObservationDate'):
-        result.append([k, sum(v['Confirmed']), sum(v['Deaths']),
-                       sum(v['Recovered']), sum(v['Active'])])
-    cases = pd.DataFrame(result, columns=['ObservationDate',
-                                          'Confirmed',
-                                          'Deaths', 'Recovered', 'Active'])
+    fig = [go.Choropleth(locations=df['Country/Region'],
+                         z=df[col].astype(float),
+                         locationmode='country names',
+                         colorbar_title=title,
+                         text=df['Country/Region'],
+                         reversescale=True,
+                         marker_line_width=0.5).update(autocolorscale=True,
+                                                       hovertemplate=df['Deaths'])]
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
 
 
+def plot_daywise_bar(url):
+    df = pd.read_csv(url)
+    date = pd.to_datetime(df['Date'])
+    fig = [go.Bar(x=date, y=df['Confirmed'], name='Confirmed'),
+           go.Bar(x=date, y=df['New cases'], name='New Cases'),
+           go.Bar(x=date, y=df['Deaths'], name='Deaths'),
+           go.Bar(x=date, y=df['Active'], name='Active'),
+           go.Bar(x=date, y=df['Recovered'], name='Recovered'),
+           go.Bar(x=date, y=df['New recovered'], name='New recovered'),
+           go.Figure().update_layout(barmode='stack', title='Day Wise COVID Spread')
+           ]
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
 
-def getResponse():
-    # URL of the api where we want to extract data from
-    url = "https://covid-193.p.rapidapi.com/statistics"
 
-    # including secret keys into header of the request
-    headers = {
-        'x-rapidapi-host': "covid-193.p.rapidapi.com",
-        'x-rapidapi-key': "d977ce9798msh4e5fe441381dc13p1addf4jsn5ad3eadb6644"
-    }
+def plot_daywise_line(url):
+    df = pd.read_csv(url)
+    date = pd.to_datetime(df['Date'])
+    fig = [go.Line(x=date, y=df['New cases'], mode='lines', name='New cases'),
+           go.Line(x=date, y=df['Confirmed'], mode='lines', name='Confirmed'),
+           go.Line(x=date, y=df['Deaths'], name='Deaths'),
+           go.Line(x=date, y=df['Active'], name='Active'),
+           go.Line(x=date, y=df['Recovered'], name='Recovered'),
+           go.Line(x=date, y=df['New recovered'], name='New recovered'),
+           go.Figure().update_layout(title='Day Wise COVID Spread', xaxis_title="", yaxis_title="")]
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
 
-    response = requests.request("GET", url, headers=headers)
-    # return received response in json format
-    return response.json()['response']
+
+def top_twenty_hbar(url, col, n, hover_data=[]):
+    df = pd.read_csv(url)
+    df = df.sort_values(col).tail(n)
+    fig = [go.Bar(x=df[col],
+                  y=df['Country/Region'],
+                  hovertext=hover_data,
+                  orientation='h',
+                  marker_color=df[col]
+                  )]
+    graphJson = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJson
 
 
-def extractData(data):
-    """ Column Names whihc we got in response from the api.
-                'continent',
-               'country',
-               'population',
-               'new_cases', 'active_cases', 'critical_cases', 'recovered_cases', '1M_po_cases', 'total_cases',
-               'new_deaths', '1M_pop_deaths', 'total_deaths',
-               '1M_pop_tests', 'total_tests',
-               'day',
-               'time']"""
-    # Creating empty lists for each column
-    continents = []
-    country = []
-    population = []
-    new_cases = []
-    active_cases = []
-    critical_cases = []
-    recovered_cases = []
-    M1_pop_cases = []
-    total_cases = []
-    new_deaths = []
-    M1_pop_deaths = []
-    total_deaths = []
-    M1_pop_tests = []
-    total_tests = []
-    day = []
-    time = []
+def plot_hbar_wm(url, col, n, min_pop=1000000):
+    df = pd.read_csv(url)
+    df = df[df['Population'] > min_pop]
+    df = df.sort_values(col, ascending=True).tail(n)
+    fig = [go.Bar(
+        x=df[col],
+        y=df['Country/Region'],
+        marker_color=df[col],
+        orientation='h',
+        text=col + '(Only Countries with > 1M Population)'
+    )]
 
-    # filling the data into each column separately
-    for i in range(len(data)):
-        continents.append(data[i]['continent'])
-        country.append(data[i]['country'])
-        population.append(data[i]['population'])
-        new_cases.append(data[i]['cases']['new'])
-        active_cases.append(data[i]['cases']['active'])
-        critical_cases.append(data[i]['cases']['critical'])
-        recovered_cases.append(data[i]['cases']['recovered'])
-        M1_pop_cases.append(data[i]['cases']['1M_pop'])
-        total_cases.append(data[i]['cases']['total'])
-        new_deaths.append(data[i]['deaths']['new'])
-        M1_pop_deaths.append(data[i]['deaths']['1M_pop'])
-        total_deaths.append(data[i]['deaths']['total'])
-        M1_pop_tests.append(data[i]['tests']['1M_pop'])
-        total_tests.append(data[i]['tests']['total'])
-        day.append(data[i]['day'])
-        time.append(data[i]['time'])
-    # creating a dictionary of all columns in which keys are the columns and values are the rows
-    data_frame = {'continents': continents, 'country': country, 'population': population, 'new_cases': new_cases,
-                  'active_cases': active_cases, 'critical_cases': critical_cases, 'recovered_cases': recovered_cases,
-                  '1M_pop_cases': M1_pop_cases, 'total_cases': total_cases, 'new_deaths': new_deaths,
-                  '1M_pop_deaths': M1_pop_deaths,
-                  'total_deaths': total_deaths, '1M_pop_tests': M1_pop_tests, 'total_tests': total_tests, 'day': day,
-                  'time': time}
-    # converting the data_frame dictionary into pandas data-frame
-    df = pd.DataFrame(data_frame)
-    # return the data-frame
-    return df
+    graphJson = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJson
 
